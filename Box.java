e676bd98a75780e72da51d5893ecbd2dff21205d
simmsb3 <brendan.simms3@mail.dcu.ie>
2023-03-02 16:43:00 +0000

    Got Box to run and take a delivery list

--- Box.java
+++ Box.java
@@ -2,53 +2,42 @@ import java.util.*;
 
 public class Box {
     static int space = 50;
+    public List<Books> BooksInBox = new ArrayList<Books>();
 
-    public List<Books> FillBox(Delivery DeliveryList) {
+
+    // Need Function to print contents of Box
+
+    public List<Books> FillBox(List<Books> DeliveryList) {
         int i = 0;
-        List<Books> BooksInBox = new ArrayList<Books>();
-        
-        System.out.print("Yuppppaa");
 
-        while (BooksInBox.size() <= 50) {
             if (DeliveryList.size() != 0) 
-            System.out.print("Conor");
             {
                 while (i < DeliveryList.size()) 
                 {
-                    System.out.print("Is");
+                    Books x = DeliveryList.get(i);
+                    
+                    BooksInBox.add(x);
+                    
                     i++;
                 }
             }
-            System.out.print("A");
-        }
 
-        System.out.print("Hero");
         return BooksInBox;
     }
 
-    public static List<Books> CreateNewBox() {
-        List<Books> box = (List<Books>) new Box();
+    public static Box CreateNewBox() {
+        Box box = new Box();
 
         return box;
     }
 
-    public void main(String [] args) 
+    public static void main(String [] args) 
     {
-        List<Books> Box_1 = CreateNewBox();
+        Box Box_1 = CreateNewBox();
         Delivery Delivery = new Delivery();
-        Box_1 = FillBox(Delivery);
-        Delivery Delivery_2 = new Delivery();
-        Box_1 = FillBox(Delivery_2);
-        Delivery Delivery_3 = new Delivery();
-        Box_1 = FillBox(Delivery_3);
-        Delivery Delivery_4 = new Delivery();
-        Box_1 = FillBox(Delivery_4);
-        Delivery Delivery_5 = new Delivery();
-        Box_1 = FillBox(Delivery_5);
-        Delivery Delivery_6 = new Delivery();
-        Box_1 = FillBox(Delivery_6);
 
-        System.out.print(Box_1);
+        Box_1.FillBox(Delivery.GenerateDelivery());
+
     }
 
 }
